85463c4f91fcc936bff301fa32857aecca6b28f1
MP_W <innovativelua@gmail.com>
2024-10-11 10:41:26 -0500

    commented controller files.

--- include/headers/brain/controller.hpp
+++ include/headers/brain/controller.hpp
@@ -1,9 +1,14 @@
 #pragma once
+
+//Required vex header.
 #include "main.h"
+
+//A global unique_ptr to be used by any file to access the main controller.
 extern std::unique_ptr<pros::Controller> mainController;
 
-constexpr int INTAKE_FORWARD = pros::E_CONTROLLER_DIGITAL_R1;
-constexpr int INTAKE_REVERSE = pros::E_CONTROLLER_DIGITAL_R2;
-constexpr int LIFT_FORWARD = pros::E_CONTROLLER_DIGITAL_L1;
-constexpr int LIFT_REVERSE = pros::E_CONTROLLER_DIGITAL_L2;
-constexpr int MOGO_TOGGLE = pros::E_CONTROLLER_DIGITAL_A;
+//Buttons to be used by each mechanism.
+constexpr int INTAKE_FORWARD = pros::E_CONTROLLER_DIGITAL_R1; //R1
+constexpr int INTAKE_REVERSE = pros::E_CONTROLLER_DIGITAL_R2; //R2
+constexpr int LIFT_FORWARD = pros::E_CONTROLLER_DIGITAL_L1; //L1
+constexpr int LIFT_REVERSE = pros::E_CONTROLLER_DIGITAL_L2; //L2
+constexpr int MOGO_TOGGLE = pros::E_CONTROLLER_DIGITAL_A; //A
--- src/brain/controller.cpp
+++ src/brain/controller.cpp
@@ -1,3 +1,5 @@
+//Respective header file.
 #include "headers/brain/controller.hpp"
 
+//A global unique pointer to be used by any file to access the main controller.
 std::unique_ptr<pros::Controller> mainController = std::make_unique<pros::Controller>(CONTROLLER_MASTER); 
