e01e229c904adeec4ad5308014123b6c851c5470
MP_W <innovativelua@gmail.com>
2024-10-11 10:36:32 -0500

    Commented the ports file.

--- include/headers/brain/ports.hpp
+++ include/headers/brain/ports.hpp
@@ -1,4 +1,8 @@
 #pragma once
+//Header which includes all the ports.
+//Ports with a negative number correspond to reversing the motor.
+
+//Motors
 constexpr int RIGHT_FRONT_MOTOR = 1;
 constexpr int RIGHT_BACK_MOTOR = -3;
 constexpr int RIGHT_TOP_MOTOR = 4;
@@ -8,9 +12,13 @@ constexpr int LEFT_TOP_MOTOR = -7;
 constexpr int LIFT_MOTOR = 8;
 constexpr int INTAKEMOTOR = 9;
 constexpr int INTAKEMOTOR2 = -10;
+
+//Sensors
 constexpr int RINGDETECTOR = 11;
 constexpr char ODOMETRY_AUX_TOP = 'A';
 constexpr char ODOMETRY_AUX_BOTTOM = 'B';
 constexpr char ODOMETRY_LEFT_TOP = 'C';
 constexpr char ODOMETRY_LEFT_BOTTOM = 'D';
+
+//Solenoids
 constexpr char MOGO_MECH_SOLENOID = 'F';
