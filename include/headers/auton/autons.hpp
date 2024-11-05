814f2e6acc0cdfe7f47c09b1229ab1a7e8fe710c
MP_W <innovativelua@gmail.com>
2024-11-04 19:29:55 -0600

    Updated Auton

--- include/headers/auton/autons.hpp
+++ include/headers/auton/autons.hpp
@@ -1,6 +1,7 @@
 #include "headers/mechs/chassis/basechassis.hpp"
 #include "headers/mechs/intake.hpp"
 #include "headers/mechs/MOGOmech.hpp"
+#include "headers/mechs/lift.hpp"
 
 void test();
 void redAWP();
