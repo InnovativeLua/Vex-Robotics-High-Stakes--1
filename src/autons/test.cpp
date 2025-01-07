418b9f4cfa330465a8b6b9ca9a28f53f43979568
MP_W <innovativelua@gmail.com>
2025-01-06 20:01:47 -0600

    Updated Skills Autonomous

--- src/autons/test.cpp
+++ src/autons/test.cpp
@@ -11,64 +11,64 @@ void skills(){
     AutonWaitFor(100.0);
     masterIntake.stop();
 
-    masterChassis.startMainPID({18.0, 0}, 75.0, false);
+    masterChassis.startMainPID({18.0, 0}, 127.0, false);
     masterChassis.distancePID.setExitCondition(2.0, 200.0, 2000, 500);
     WaitChassisPID();
 
-    masterChassis.startHeadingPID(270.0, 50.0);
-    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 1000);
+    masterChassis.startHeadingPID(270.0, 100.0);
+    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 300);
     WaitChassisPID();
 
-    masterChassis.startMainPID({18.0, 20.0}, 75.0, true);
-    masterChassis.distancePID.setExitCondition(2.0, 200.0, 4000, 500);
+    masterChassis.startMainPID({17.0, 20.0}, 127.0, true);
+    masterChassis.distancePID.setExitCondition(2.0, 200.0, 3000, 500);
     WaitChassisPID();
     masterMogo.retract();
     AutonWaitFor(100.0);
 
-    masterChassis.startHeadingPID(0.0, 50.0);
-    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 1000);
+    masterChassis.startHeadingPID(0.0, 100.0);
+    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 300);
     WaitChassisPID();
 
-    masterChassis.startMainPID({45.0, 25.0}, 75.0, false);
+    masterChassis.startMainPID({45.0, 25.0}, 127.0, false);
     masterChassis.distancePID.setExitCondition(2.0, 200.0, 4000, 500);
     masterIntake.spinForward();
     WaitChassisPID();
 
-    masterChassis.startHeadingPID(95.0, 50.0);
-    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 1000);
+    masterChassis.startHeadingPID(90.0, 100.0);
+    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 300);
     WaitChassisPID();
 
-    masterChassis.startMainPID({40.0, 65.0}, 75.0, false);
+    masterChassis.startMainPID({42.0, 65.0}, 127.0, false);
     masterChassis.distancePID.setExitCondition(2.0, 200.0, 4000, 500);
     WaitChassisPID();
 
-    masterChassis.startMainPID({40.0, 54.0}, 50.0, true);
+    masterChassis.startMainPID({42.0, 54.0}, 100.0, true);
     masterChassis.distancePID.setExitCondition(2.0, 200.0, 4000, 500);
     WaitChassisPID();
 
-    masterChassis.startHeadingPID(180.0, 50.0);
-    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 1000);
+    masterChassis.startHeadingPID(180.0, 100.0);
+    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 300);
     WaitChassisPID();
 
-    masterChassis.startMainPID({7.0, 60.0}, 75.0, false);
+    masterChassis.startMainPID({10.0, 55.0}, 127.0, false);
     masterChassis.distancePID.setExitCondition(2.0, 200.0, 4000, 500);
     WaitChassisPID();
 
-    masterChassis.startMainPID({-5.0, 60.0}, 50.0, false);
+    masterChassis.startMainPID({-3.0, 55.0}, 100.0, false);
     masterChassis.distancePID.setExitCondition(1.0, 200.0, 4000, 500);
     WaitChassisPID();
 
     AutonWaitFor(500.0);
 
-    masterChassis.startMainPID({15.0, 60.0}, 50.0, true);
+    masterChassis.startMainPID({20.0, 59.0}, 100.0, true);
     masterChassis.distancePID.setExitCondition(2.0, 200.0, 4000, 500);
     WaitChassisPID();
 
-    masterChassis.startHeadingPID(90.0, 50.0);
-    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 1000);
+    masterChassis.startHeadingPID(90.0, 100.0);
+    masterChassis.headingPID.setExitCondition(0.01, 500.0, 3000, 300);
     WaitChassisPID();
 
-    masterChassis.startMainPID({15.0, 85.0}, 50.0, false);
+    masterChassis.startMainPID({18.0, 85.0}, 100.0, false);
     masterChassis.distancePID.setExitCondition(2.0, 200.0, 4000, 500);
     WaitChassisPID();
 
